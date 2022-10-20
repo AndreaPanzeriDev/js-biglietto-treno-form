b5656580f2541f699f939604e583ee22d834036d
AndreaPanzeriDev <andpanz@libero.it>
2022-10-20 17:32:02 +0200

    output complete

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,21 +7,37 @@ function Calculator(){
     console.log(NameSurname);
     let Km = parseInt(document.getElementById("inputKm").value);
     console.log(Km);
-    let age = document.getElementById("inputGroupSelect02").value;
+    let age = parseInt(document.getElementById("inputGroupSelect02").value);
     console.log(age);
     let Price;
+    //output nome e cognome biglietto
+    document.getElementById('outputName').innerHTML = NameSurname;
+    //output tipologia biglietto
+    document.getElementById('outputOffer').innerHTML = 'Biglietto Standard';
+    //output numero carrozza suppongo 12 carrozze
+    let wagon = Math.random() * 12;
+    document.getElementById('outputWagon').innerHTML = wagon.toFixed(0);
+    //output codice cp
+    let cp_code = Math.random() * 9999;
+    document.getElementById('outputCp').innerHTML = cp_code.toFixed(0);
 
     switch (age){
-        case '1':
+        case 1:
             Price = (Km * KmPrice) * 0.8;
             console.log(Price);
-        case '2':
+            document.getElementById('outputPrice').innerHTML = Price.toFixed(2);
+            break;
+        case 2:
             Price = (Km * KmPrice) * 0.6;
             console.log(Price);
+            document.getElementById('outputPrice').innerHTML = Price.toFixed(2);
+            break;
         
-        case '3':
+        case 3:
             Price = Km * KmPrice;
             console.log(Price);
+            document.getElementById('outputPrice').innerHTML = Price.toFixed(2) + '$';
+            break;
     }
     
 }
